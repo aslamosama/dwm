0dd7043a8e8a587f65ceb58a99cc9718928e8306
aslamosama <90507714+aslamosama@users.noreply.github.com>
2025-03-11 14:28:46 +0500

    keep xresources disabled and rely on xrdb for now

--- config.h
+++ config.h
@@ -11,7 +11,7 @@
 static const unsigned int borderpx       = 0;   /* border pixel of windows */
 static const int corner_radius           = 10;
 #else
-static const unsigned int borderpx       = 1;   /* border pixel of windows */
+static unsigned int borderpx             = 1;   /* border pixel of windows */
 #endif // ROUNDED_CORNERS_PATCH
 #if BAR_BORDER_PATCH
 /* This allows the bar border size to be explicitly set separately from borderpx.
@@ -52,7 +52,7 @@ static const int showbar                 = 0;   /* 0 means no bar */
 #else
 static const int showbar                 = 1;   /* 0 means no bar */
 #endif // BAR_HOLDBAR_PATCH
-static const int topbar                  = 0;   /* 0 means bottom bar */
+static int topbar                  = 0;   /* 0 means bottom bar */
 #if TAB_PATCH
 /*  Display modes of the tab bar: never shown, always shown, shown only in  */
 /*  monocle mode in the presence of several windows.                        */
@@ -671,8 +671,8 @@ static const BarRule barrules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
-static const int nmaster     = 1;    /* number of clients in master area */
+static float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
+static int nmaster     = 1;    /* number of clients in master area */
 #if FLEXTILE_DELUXE_LAYOUT
 static const int nstack      = 0;    /* number of clients in primary stack area */
 #endif // FLEXTILE_DELUXE_LAYOUT
@@ -955,6 +955,129 @@ static const Key on_empty_keys[] = {
   {0,         XK_v,          spawn,      {.v = (const char*[]){"st", "men", NULL } } },
 };
 #endif // ON_EMPTY_KEYS_PATCH
+
+#if XRESOURCES_PATCH
+/*
+* Xresources preferences to load at startup.
+*
+*     Name                      Type       Address
+*    ------------------------------------------------
+*     "nmaster"                 INTEGER    &nmaster
+*     "mfact"                   FLOAT      &mfact
+*     "color1"                  STRING     &color1
+*
+* In the Xresources file setting resources shoud be prefixed with "dwm.", e.g.
+*
+*    dwm.nmaster: 1
+*    dwm.mfact: 0.50
+*    dwm.color1: #FA6EFA
+*
+* Note that the const qualifier must be removed from the variables if you plan on
+* overriding them with values from Xresources. While resources can be reloaded
+* using the xrdb function some changes may only take effect following a restart.
+*/
+
+ResourcePref resources[] = {
+	/* Resource name            Type       Address                */
+  { "borderpx",               INTEGER,   &borderpx },
+  { "topbar",                 INTEGER,   &topbar },
+  { "nmaster",                INTEGER,   &nmaster },
+  { "mfact",                  FLOAT,     &mfact },
+  /* { "color0",                 STRING,    &termcolor0 }, */
+  /* { "color1",                 STRING,    &termcolor1 }, */
+  /* { "color2",                 STRING,    &termcolor2 }, */
+  /* { "color3",                 STRING,    &termcolor3 }, */
+  /* { "color4",                 STRING,    &termcolor4 }, */
+  /* { "color5",                 STRING,    &termcolor5 }, */
+  /* { "color6",                 STRING,    &termcolor6 }, */
+  /* { "color7",                 STRING,    &termcolor7 }, */
+  /* { "color8",                 STRING,    &termcolor8 }, */
+  /* { "color9",                 STRING,    &termcolor9 }, */
+  /* { "color10",                STRING,    &termcolor10 }, */
+  /* { "color11",                STRING,    &termcolor11 }, */
+  /* { "color12",                STRING,    &termcolor12 }, */
+  /* { "color13",                STRING,    &termcolor13 }, */
+  /* { "color14",                STRING,    &termcolor14 }, */
+  /* { "color15",                STRING,    &termcolor15 }, */
+	{ "normfgcolor",            STRING,    &normfgcolor },
+	{ "normbgcolor",            STRING,    &normbgcolor },
+	{ "normbordercolor",        STRING,    &normbordercolor },
+	{ "normfloatcolor",         STRING,    &normfloatcolor },
+	{ "selfgcolor",             STRING,    &selfgcolor },
+	{ "selbgcolor",             STRING,    &selbgcolor },
+	{ "selbordercolor",         STRING,    &selbordercolor },
+	{ "selfloatcolor",          STRING,    &selfloatcolor },
+	{ "titlenormfgcolor",       STRING,    &titlenormfgcolor },
+	{ "titlenormbgcolor",       STRING,    &titlenormbgcolor },
+	{ "titlenormbordercolor",   STRING,    &titlenormbordercolor },
+	{ "titlenormfloatcolor",    STRING,    &titlenormfloatcolor },
+	{ "titleselfgcolor",        STRING,    &titleselfgcolor },
+	{ "titleselbgcolor",        STRING,    &titleselbgcolor },
+	{ "titleselbordercolor",    STRING,    &titleselbordercolor },
+	{ "titleselfloatcolor",     STRING,    &titleselfloatcolor },
+	{ "tagsnormfgcolor",        STRING,    &tagsnormfgcolor },
+	{ "tagsnormbgcolor",        STRING,    &tagsnormbgcolor },
+	{ "tagsnormbordercolor",    STRING,    &tagsnormbordercolor },
+	{ "tagsnormfloatcolor",     STRING,    &tagsnormfloatcolor },
+	{ "tagsselfgcolor",         STRING,    &tagsselfgcolor },
+	{ "tagsselbgcolor",         STRING,    &tagsselbgcolor },
+	{ "tagsselbordercolor",     STRING,    &tagsselbordercolor },
+	{ "tagsselfloatcolor",      STRING,    &tagsselfloatcolor },
+	{ "hidnormfgcolor",         STRING,    &hidnormfgcolor },
+	{ "hidnormbgcolor",         STRING,    &hidnormbgcolor },
+	{ "hidselfgcolor",          STRING,    &hidselfgcolor },
+	{ "hidselbgcolor",          STRING,    &hidselbgcolor },
+	{ "urgfgcolor",             STRING,    &urgfgcolor },
+	{ "urgbgcolor",             STRING,    &urgbgcolor },
+	{ "urgbordercolor",         STRING,    &urgbordercolor },
+	{ "urgfloatcolor",          STRING,    &urgfloatcolor },
+	#if RENAMED_SCRATCHPADS_PATCH
+	{ "scratchselfgcolor",      STRING,    &scratchselfgcolor },
+	{ "scratchselbgcolor",      STRING,    &scratchselbgcolor },
+	{ "scratchselbordercolor",  STRING,    &scratchselbordercolor },
+	{ "scratchselfloatcolor",   STRING,    &scratchselfloatcolor },
+	{ "scratchnormfgcolor",     STRING,    &scratchnormfgcolor },
+	{ "scratchnormbgcolor",     STRING,    &scratchnormbgcolor },
+	{ "scratchnormbordercolor", STRING,    &scratchnormbordercolor },
+	{ "scratchnormfloatcolor",  STRING,    &scratchnormfloatcolor },
+	#endif // RENAMED_SCRATCHPADS_PATCH
+	#if BAR_FLEXWINTITLE_PATCH
+	{ "normTTBbgcolor",         STRING,    &normTTBbgcolor },
+	{ "normLTRbgcolor",         STRING,    &normLTRbgcolor },
+	{ "normMONObgcolor",        STRING,    &normMONObgcolor },
+	{ "normGRIDbgcolor",        STRING,    &normGRIDbgcolor },
+	{ "normGRD1bgcolor",        STRING,    &normGRD1bgcolor },
+	{ "normGRD2bgcolor",        STRING,    &normGRD2bgcolor },
+	{ "normGRDMbgcolor",        STRING,    &normGRDMbgcolor },
+	{ "normHGRDbgcolor",        STRING,    &normHGRDbgcolor },
+	{ "normDWDLbgcolor",        STRING,    &normDWDLbgcolor },
+	{ "normSPRLbgcolor",        STRING,    &normSPRLbgcolor },
+	{ "normfloatbgcolor",       STRING,    &normfloatbgcolor },
+	{ "actTTBbgcolor",          STRING,    &actTTBbgcolor },
+	{ "actLTRbgcolor",          STRING,    &actLTRbgcolor },
+	{ "actMONObgcolor",         STRING,    &actMONObgcolor },
+	{ "actGRIDbgcolor",         STRING,    &actGRIDbgcolor },
+	{ "actGRD1bgcolor",         STRING,    &actGRD1bgcolor },
+	{ "actGRD2bgcolor",         STRING,    &actGRD2bgcolor },
+	{ "actGRDMbgcolor",         STRING,    &actGRDMbgcolor },
+	{ "actHGRDbgcolor",         STRING,    &actHGRDbgcolor },
+	{ "actDWDLbgcolor",         STRING,    &actDWDLbgcolor },
+	{ "actSPRLbgcolor",         STRING,    &actSPRLbgcolor },
+	{ "actfloatbgcolor",        STRING,    &actfloatbgcolor },
+	{ "selTTBbgcolor",          STRING,    &selTTBbgcolor },
+	{ "selLTRbgcolor",          STRING,    &selLTRbgcolor },
+	{ "selMONObgcolor",         STRING,    &selMONObgcolor },
+	{ "selGRIDbgcolor",         STRING,    &selGRIDbgcolor },
+	{ "selGRD1bgcolor",         STRING,    &selGRD1bgcolor },
+	{ "selGRD2bgcolor",         STRING,    &selGRD2bgcolor },
+	{ "selGRDMbgcolor",         STRING,    &selGRDMbgcolor },
+	{ "selHGRDbgcolor",         STRING,    &selHGRDbgcolor },
+	{ "selDWDLbgcolor",         STRING,    &selDWDLbgcolor },
+	{ "selSPRLbgcolor",         STRING,    &selSPRLbgcolor },
+	{ "selfloatbgcolor",        STRING,    &selfloatbgcolor },
+	#endif // BAR_FLEXWINTITLE_PATCH
+};
+#endif // XRESOURCES_PATCH
 
 static const Key keys[] = {
   /* modifier                     key            function                argument */
